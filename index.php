a140c2505b0edeb4b54f6525fb49bd552a07c59c
tz-andy <18396813806@163.com>
2017-09-18 14:04:57 +0800

    初始化v1.0.0(登录页结束)

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$dir = 'main';
+$filename = 'index';
+if(array_key_exists('PATH_INFO',$_SERVER)){
+    $path = $_SERVER['PATH_INFO'];
+    $str = substr($path,1);
+    $ret = explode('/',$str);
+    if(count($ret) == 2){
+        $dir = $ret[0];
+        $filename = $ret[1];
+    }else{
+        $filename = 'login';
+    }
+}
+include('./views/'.$dir.'/'.$filename.'.html');
+?>
