82566529b7988dc8d148c842a19eb4c142f2f951
tz-andy <18396813806@163.com>
2017-09-27 18:18:27 +0800

    个人中心表单提交功能完成

--- public/js/config.js
+++ public/js/config.js
@@ -14,6 +14,7 @@ require.config({
         form : 'jquery-form/jquery.form',
         uploadify : 'uploadify/jquery.uploadify',
         region : 'jquery-region/jquery.region',
+        ckeditor : 'ckeditor/ckeditor',
         common : '../js/common',
         login : '../js/login',
         teacherList : '../js/teacherList',
@@ -33,6 +34,9 @@ require.config({
         },
         uploadify : {
             deps : ['jquery']
+        },
+        ckeditor : {
+            exports : 'CKEDITOR'
         }
     }
 });
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','uploadify','region'],function ($,template) {
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language','validate','form'],function ($,template,CKEDITOR) {
     //获取数据，渲染页面
     $.ajax({
        type : 'get',
@@ -23,9 +23,69 @@ define(['jquery','template','uploadify','region'],function ($,template) {
                     $('.preview img').attr('src',imgObj.result.path);
                 }
             });
+            //日期插件
+            $('#birthday').datepicker({
+                'language' : 'zh-CN',
+                'format' : 'yyyy-mm-dd',
+                'endDate' : '0d'
+            });
+            $('#joinDate').datepicker({
+                'language' : 'zh-CN',
+                'format' : 'yyyy-mm-dd',
+                'endDate' : '0d'
+            });
             //省市县三级联动
             $('#pcd').region({
                url : '/public/assets/jquery-region/region.json'
+            });
+            //富文本
+            CKEDITOR.replace('editor');
+            //表单提交
+            $('#setInfo').validate({
+               sendForm : false,
+                description : {
+                   roster : {
+                       required : '请填写昵称',
+                   },
+                    birthday : {
+                        required : '出生日期不能为空'
+                    },
+                    cellphone : {
+                       required : '手机号码不能为空',
+                        pattern : '请填写正确的手机号码'
+                    },
+                   email : {
+                        required : '邮箱不能为空',
+                        pattern : '请填写正确的邮箱'
+                    },
+                    joinDate : {
+                        required: '入职日期不能为空'
+                    }
+                },
+                valid : function () {
+                   //获取籍贯信息
+                    var p = $('#p').find('option:selected').text();
+                    var c = $('#c').find('option:selected').text();
+                    var d = $('#d').find('option:selected').text();
+                   var tcHometown = p + '|' + c + '|' + d;
+                   //更新富文本内容
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    $(this).ajaxSubmit({
+                        type : 'post',
+                        url : '/api/teacher/modify',
+                        data : {
+                            tc_hometown : tcHometown
+                        },
+                        dataType : 'json',
+                        success : function (data) {
+                            if(data.code==200){
+                                location.reload();
+                            }
+                        }
+                    });
+                }
             });
         }
     });
